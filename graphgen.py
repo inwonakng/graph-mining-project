488205a8af98bd12a85de95464895e5b09384f67
inwonakng <inwon.kang04@gmail.com>
2020-04-25 22:41:19 -0400

    added to graph

--- graphgen.py
+++ graphgen.py
@@ -2,49 +2,59 @@ import matplotlib.pyplot as plt
 import numpy as np
 # right now this is just for dataset 1
 acc_types = ['Total','White victory','Black victory','Draw']
-test_types = ['Common Neighbors','Edge Weights', 'Number Paths (3)', 'Number Paths (4)', 'Pageranks (100)', 'Pageranks (1000)']
-common_neighbors_acc = [55.51,	58.43,	54.99,	4.62]
-edge_weights_acc = [58.24,	60.08,	56.55,	7.27]
-num_paths_3_acc = [45.39,	58.3,	54.42,	4.13]
-num_paths_4_acc = [55.62,	58.5,	54.96,	4.86]
+test_types = ['Common Neighbors','Common Neighbors (Colors)','Edge Weights','Edge Weights (Colors)',
+            'Number Paths (3)', 'Number Paths (4)','Opening Moves', 'Pageranks (100)', 'Pageranks (1000)']
+common_neighbors_acc = [55.31,	57.55,	55.49,	3.47]
+ccc_acc = [55.25,	56.63,	57.06,	3.45]
+edge_weights_acc = [58.22,	59.52,	57.04,	4.65]
+ewc_acc = [57.69,	57.87,	57.78,	2]
+num_paths_3_acc = [44.9,	56.4,	54.4,	5.16]
+num_paths_4_acc = [55.26,	57.52,	55.4,	3.53]
+om_acc = [43.56,	55.97,	53.65,	3.32]
 
-a = [11.93,	50.94,	47.23,	3.56]
-b = [11.93,	48.47,	46.59,	3.56]
-c = [12.36,	51.71,	47.08,	3.59]
+a = [13.13,	51.7,	48.55,	3.67]
+b = [13.58,	52.25,	50.12,	3.72]
+c = [13.29,	51.78,	50.18,	3.61]
 
 pageranks_100 = [(i+j+k)/3 for i,j,k in zip(a,b,c)]
 
-aa = [34.68,	51.69,	48.38,	3.55]
-bb = [34.18,	52.01,	48.52,	3.54]
-cc = [34.48,	51.66,	48.62,	3.42]
+aa = [36.75,	52.49,	50.08,	3.4]
+bb = [36.8,	52.48,	50.21,	3.43]
+cc = [36.04,	52.34,	49.96,	3.58]
 
 pageranks_1000 = [(i+j+k)/3 for i,j,k in zip(aa,bb,cc)]
 
-data = [common_neighbors_acc,edge_weights_acc,num_paths_3_acc,num_paths_4_acc,pageranks_100,pageranks_1000]
+data = [common_neighbors_acc,ccc_acc, edge_weights_acc,ewc_acc ,num_paths_3_acc,num_paths_4_acc,om_acc,pageranks_100,pageranks_1000]
 X = np.arange(4)
-fig = plt.figure()
+fig = plt.figure(figsize = (10,7))
 ax = fig.add_subplot(111)
 
-ax.set_title('Accuracy values for different methods (Dataset 1)')
+ax.set_title('Accuracy values for different methods (Dataset 3)')
 ax.set_ylabel('Percentage')
 
-l0 = ax.bar(X+0.00,data[0],color = 'c',width = 0.15)
+l0 = ax.bar(X+0.00,data[0],color = 'c',width = 0.10)
 
-l1 = ax.bar(X+0.15,data[1],color = '#6e2aeb',width = 0.15)
+l1 = ax.bar(X+0.10,data[1],color = '#6e2aeb',width = 0.10)
 
-l2 = ax.bar(X+0.30,data[2],color = '#ebb12a',width = 0.15)
+l2 = ax.bar(X+0.20,data[2],color = '#ebb12a',width = 0.10)
 
-l3 = ax.bar(X+0.45,data[3],color = '#eb2adb',width = 0.15)
+l3 = ax.bar(X+0.30,data[3],color = '#eb2adb',width = 0.10)
 
-l4 = ax.bar(X+0.60,data[4],color = '#e3e032',width = 0.15)
+l4 = ax.bar(X+0.40,data[4],color = '#e3e032',width = 0.10)
 
-l5 = ax.bar(X+0.75,data[5],color = '#2dc25c',width = 0.15)
+l5 = ax.bar(X+0.50,data[5],color = '#2dc25c',width = 0.10)
 
-ax.legend((l0,l1,l2,l3,l4,l5),(test_types))
+l6 = ax.bar(X+0.60,data[6],color = 'r',width = 0.10)
+
+l7 = ax.bar(X+0.70,data[7],color = '#42b6f5',width = 0.10)
+
+l8 = ax.bar(X+0.80,data[8],color = '#d7f542',width = 0.10)
+
+ax.legend((l0,l1,l2,l3,l4,l5,l6,l7,l8),(test_types))
 
 ax.set_xticks( np.arange(0,4)+.375)
 ax.set_xticklabels(acc_types)
 ax.set_yticks(np.arange(0,101,10))
 
 # plt.show()
-plt.savefig('imgs/dataset1.png')
+plt.savefig('imgs/dataset3.png')
